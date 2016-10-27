21811a073ff59821ca7daed239eb9fc7feb06cf0
Buddhika <Buddhika@LAPTOP-659KAUKK>
2016-10-27 10:25:29 +0530

    Commit

---
+++ 70-483_C_Sharp/Static/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car.Accelerate();
+
+            Bicycle b = new Bicycle();
+            Console.ReadKey();
+        }
+    }
+
+    class Bicycle
+    {
+        public Bicycle()
+        {
+            Console.WriteLine("Bicycle - instance constructor");
+        }
+
+        static Bicycle()
+        {
+            Console.WriteLine("Bicycle - static constructor");
+        }
+    }
+
+    // if the class is static, all its members needs to be static
+    static class Car
+    {
+        private static string brand;
+        static Car()
+        {
+            Console.WriteLine("car static constructor");
+        }        
+
+        public static string Brand
+        {
+            get
+            {
+                return brand;
+            }
+
+            set
+            {
+                brand = value;
+            }
+        }
+
+        public static void Accelerate()
+        {
+            Console.WriteLine("Accelereate");
+        }
+    }
+}
