6640e01120259bfd7d18cfb1a1f4b8a35b49258e
Buddhika <Buddhika@LAPTOP-659KAUKK>
2016-10-28 10:24:31 +0530

    indexes

---
+++ 70-483_C_Sharp/Boxing and Unboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Boxing_and_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            object o = i;       // Boxing - implicit cast
+            Console.WriteLine("boxing : " + o);
+
+            o = 30;
+            i = (int) o;        // Unboxing - explicit cast required
+            Console.WriteLine("unboxing  : " + i);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 70-483_C_Sharp/Indexes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarIndexedCollection collection = new CarIndexedCollection();
+            collection[0] = "Nissan";
+            collection[1] = "Toyota";
+            collection[2] = "BMW";
+
+            Console.WriteLine("0 : " + collection[0]);
+            Console.WriteLine("9 : " + collection[9]);
+            Console.WriteLine("10: " + collection[10]);
+
+            Console.ReadKey();
+        }
+    }
+
+    class CarIndexedCollection
+    {       
+        string[] cars = new string[10];
+
+        // indexer
+        public string this[int indexVal]
+        {
+            get
+            {
+                /* return the specified index here */
+                if (indexVal >= 0 && indexVal < cars.Length)
+                {
+                    if (string.IsNullOrWhiteSpace(cars[indexVal]))
+                    { 
+                        return "Do not have a value";
+                    }
+                    else
+                    { 
+                        return cars[indexVal];
+                    }
+                }
+                else
+                {                    
+                    return "Invlaid index : " + indexVal + ". length is 0 to " + (cars.Length - 1);
+                }
+            }
+            set
+            {
+                /* set the specified index to value here */
+                if (indexVal >= 0 && indexVal < cars.Length)
+                {
+                    cars[indexVal] = value;
+                }
+            }
+        }
+    }    
+}
