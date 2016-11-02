205eb7995edbeaea8fe4edb5a9d9dda29b525036
Buddhika <Buddhika@LAPTOP-659KAUKK>
2016-11-02 10:41:48 +0530

    explicit interface implementation & IEnumerables

---
+++ 70-483_C_Sharp/Explicit Interface Implementation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Interface_Implementation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass cc = new ChildClass();
+
+            // explicit interface implemnatation calls via 
+            // explicit casting to the interface type
+            ((IFirst)cc).DoThis();
+            ((ISecond)cc).DoThis();
+            ((IFirst)cc).DoThis(6);
+            ((ISecond)cc).DoThis(20);
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IFirst
+    {
+        void DoThis();
+        void DoThis(int x);
+    }
+
+    interface ISecond
+    {
+        void DoThis();
+        int DoThis(int x);
+    }
+
+    public class ChildClass : IFirst, ISecond
+    {
+        //----------------- SAME SIGNATURE, SAME RETURN --------------------
+        void IFirst.DoThis()
+        {
+            Console.WriteLine("IFIRST - DoThis()");
+        }
+
+        void ISecond.DoThis()
+        {
+            Console.WriteLine("ISECOND - DoThis()");
+        }
+        //----------------- SAME SIGNATURE, SAME RETURN --------------------
+
+        //----------------- SAME SIGNATURE, DIFFERENT RETURN ---------------
+        void IFirst.DoThis(int x)
+        {
+            Console.WriteLine("IFIRST - DoThis(int x)");
+        }
+
+        int ISecond.DoThis(int x)
+        {
+            Console.WriteLine("ISECOND - DoThis(int x)");
+            return x;
+        }
+        //----------------- SAME SIGNATURE, DIFFERENT RETURN ---------------
+    }
+}
---
+++ 70-483_C_Sharp/I Enumerable/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> list = new List<string>();
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            foreach (string str in list)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("");
+
+            string[] array = new string[3];
+            array[0] = "A";
+            array[1] = "B";
+            array[2] = "C";
+            foreach (string str in array)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("");
+
+            // custom collection with an indexer
+            // if you want to use forach on a custom collection,
+            // you must implement IEnumerable
+            StringCollection stringCollection = new StringCollection();
+            stringCollection[0] = "A";
+            stringCollection[1] = "B";
+            stringCollection[2] = "C";
+            foreach (string str in stringCollection)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+
+    class StringCollection : IEnumerable
+    {
+        // string array
+        string[] strings = new string[3];
+
+        public string this[int index]
+        {
+            get
+            { /* return the specified index here */
+                return strings[index];
+            }
+            set
+            { /* set the specified index to value here */
+                strings[index] = value;
+            }
+        }
+
+        // implement IEnumerable - provide our own implemenation
+        public IEnumerator GetEnumerator()
+        {
+            foreach (string str in strings)
+            {
+                yield return str;
+            }
+        }
+
+        //// asking to use IEnumerator on System.Array class
+        //public IEnumerator GetEnumerator()
+        //{
+        //    return strings.GetEnumerator();
+        //}
+    }
+
+}
