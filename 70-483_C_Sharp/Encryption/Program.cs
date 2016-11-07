d8c3b3fdf3e19d8626dbb7e2a274545698ec5d7b
Buddhika <Buddhika@LAPTOP-659KAUKK>
2016-11-07 07:01:41 +0000

    Encryption, Hashing and salting

---
+++ 70-483_C_Sharp/Encryption/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = Directory.GetCurrentDirectory() + "\\MyFile.txt";
+            ReadFile(location);
+
+            // Process.Start("explorer.exe", @"c:\temp");
+            Process.Start(Directory.GetCurrentDirectory());
+            Console.WriteLine("\nPress any key to encrypt - watch file getting green on the folder");
+            Console.ReadLine();
+            EncryptFile(location);
+
+            Console.WriteLine("\nPress any key to encrypt - watch file getting black on the folder");
+            Console.ReadLine();
+            DecryptFile(location);
+        }
+
+        private static void DecryptFile(string location)
+        {
+            File.Encrypt(location);
+        }
+
+        private static void EncryptFile(string location)
+        {
+            File.Decrypt(location);
+        }
+
+        private static void ReadFile(string location)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(location);
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }        
+        }
+
+    }
+}
---
+++ 70-483_C_Sharp/Hashing with Salts/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing_with_Salts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool doAgain = false;
+            do
+            {
+                Console.Write("Enter Password : ");
+                string password = Console.ReadLine();
+
+                string hashedPassword = Hash(password);
+                Console.WriteLine("Your password : " + password);
+                Console.WriteLine("Your hashed password : " + hashedPassword);
+
+                string hashedSaltedPassword = HashWithSalt(password);
+                Console.WriteLine("Your hashed salted password : " + hashedSaltedPassword);
+
+                // user login scenario
+                Console.WriteLine("\n\n\nUser logging in again.....");
+                Console.Write("Enter Password : ");
+                password = Console.ReadLine();
+                string hashedPassword2 = Hash(password);
+                Console.WriteLine("Your hashed password : " + hashedPassword2);
+                string hashedSaltedPassword2 = HashWithSalt(password);
+                Console.WriteLine("Your hashed salted password : " + hashedSaltedPassword2);
+
+                // login hashed and salted hashed password comparison
+                if (hashedPassword == hashedPassword2 && hashedSaltedPassword == hashedSaltedPassword2)
+                {
+                    Console.WriteLine("\n\n\nLogin Success");
+                }
+                else
+                {
+                    Console.WriteLine("\n\n\nLogin Unsuccessful");
+                }
+
+                // looping
+                Console.Write("\n\n\nDO it again (y / n) : ");                
+                doAgain = Console.ReadLine().ToLower() == "y"? true : false;
+            } while (doAgain);
+            
+        }
+
+        // Hashing a string with a salt
+        private static string HashWithSalt(string password)
+        {
+            string salt = "QsG%):@2,nk";                    // salt is a hard coded random string
+            HashAlgorithm algorithm = SHA256.Create();
+            string saltedPlainPassword = salt + password;    // just add salt
+            byte[] saltedPlainPasswordByteArr = Encoding.Default.GetBytes(saltedPlainPassword);
+            byte[] hashedPasswordByteArr = algorithm.ComputeHash(saltedPlainPasswordByteArr);
+            string hashedSaltedPasword = Convert.ToBase64String(hashedPasswordByteArr);
+            return hashedSaltedPasword;
+        } 
+
+        // Hashing a string
+        private static string Hash(string password)
+        {
+            HashAlgorithm algorithm = SHA256.Create();
+            byte[] passwordByteArray = Encoding.Default.GetBytes(password);
+            byte[] hashedPasswordByteArray = algorithm.ComputeHash(passwordByteArray);
+            string hashedPassword = Convert.ToBase64String(hashedPasswordByteArray);
+            return hashedPassword;
+        }
+    }
+}
